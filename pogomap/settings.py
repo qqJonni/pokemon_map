300585f7e2e5a82304be1f6829ccd0a68b20a8c8
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-05 22:54:30 +0500

    create project

--- pogomap/settings.py
+++ pogomap/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'REPLACE_ME')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'true').lower() in ['yes', '1', 'true']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
